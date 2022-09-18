af2a84b538728db0190142de544db35574ff0985
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-09-18 18:02:17 -0400

    added the food and some comments

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
 import time
 
+# Setting up the initial background screen of the game
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("SNAKE")
+# makes it so the screen doesn't auto update makes it so the snake will look smoother
 screen.tracer(0)
 
+# Creating the snake and food objects from the classes imported
 snake = Snake()
+food = Food()
+
 screen.listen()
+
+# Allows user to use the arrow buttons to move the snake the game is listening for key presses
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.right, "Right")
 screen.onkey(snake.left, "Left")
 
+# The game code
+
 game_is_on = True
 
 while game_is_on:
+    # update the screen for the changes to show
     screen.update()
+    # A small pause in the update as moves happen
     time.sleep(0.1)
-
     snake.auto_move()
+
+    # Detect when the snake "collects" the food
+    if snake.head.distance(food) < 13:
+        food.a_new_piece()
+
 
 
 screen.exitonclick()
