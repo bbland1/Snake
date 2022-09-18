517775fec92105d710799eb0433408dd32b4208f
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-09-18 16:43:21 -0400

    made the basic moving and little snake

---
+++ main.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("SNAKE")
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.auto_move()
+
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+SNAKE_START_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self) -> None:
+        self.snake_segments = []
+        self.create_snake()
+        self.head = self.snake_segments[0]
+
+    def create_snake(self):
+        for snake_start in SNAKE_START_POSITION:
+            snake_part = Turtle("square")
+            snake_part.color("white")
+            snake_part.penup()
+            snake_part.goto(snake_start)
+            self.snake_segments.append(snake_part)
+
+    def auto_move(self):
+
+        for segment in range(len(self.snake_segments) - 1, 0, -1):
+            new_x = self.snake_segments[segment - 1].xcor()
+            new_y = self.snake_segments[segment - 1].ycor()
+            self.snake_segments[segment].goto(new_x, new_y)
+
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
